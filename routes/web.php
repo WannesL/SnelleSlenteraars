05850f070b5ae8cacdbeb93aa675a18e1fa6a0f5
Wannes Lamoot <wannes.lamoot@student.howest.be>
2026-03-14 13:39:25 +0100

    Add WandelingController

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,6 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Public\WandelingController;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [WandelingController::class, 'index'])->name('home');
